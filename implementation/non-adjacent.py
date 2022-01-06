3895112974373f10cb82d99524f3bd6c3f463b8c
Alex Foulds <alex.foulds333@gmail.com>
2022-01-06 18:18:55 +0000

    Adding print lines and comments

--- implementation/non-adjacent.py
+++ implementation/non-adjacent.py
@@ -5,15 +5,24 @@ def main():
     print(largestSum(inputArr));
 
 def largestSum(array):
-    #set initial empty subset values
+    #Set initial empty subset values
     includeSum = 0;
     excludeSum = 0;
 
     for i in array:
+        
+        print("---")
+        print("i:",i);
+        #Maximum sum of previous subset
         tempExcludeSum = max(includeSum,excludeSum);
-
+        
+        #Maximum of current subset - cant include indexof(i-1)/adjacent value
         includeSum = excludeSum + i;
+        #Maximum of current subset if i is not included
         excludeSum = tempExcludeSum;
+
+        print("includeSum:",includeSum);
+        print("excludeSum:",excludeSum);
 
     return max(includeSum,excludeSum);
 
